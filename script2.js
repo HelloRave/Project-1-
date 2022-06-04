bebc9e3a4e565ed0b1c0c8b0699eb238c0a2dbf7
HelloRave <huang.weiwei@trentglobal.edu.sg>
2022-06-04 04:40:26 +0000

    Displayed appropriate section when searched

--- script2.js
+++ script2.js
@@ -10,16 +10,27 @@ search.addEventListener('keydown', async function (event) {
     let data = await loadData()
     // let tbody = document.querySelector('tbody')
     if (event.key == 'Enter') {
+        
+        let allRows = document.querySelectorAll('tr')
+        for (let row of allRows){
+            row.style.display = ''
+        }
+
         let productNotFound = true
-        for (let individual of data) {
-            if (individual.product_name.toLowerCase().includes(search.value.toLowerCase())) {
-                console.log(individual.product_name)
+
+        let td = document.querySelectorAll('.product-name')
+        for (let i = 0; i < td.length; i++){
+            if (td[i].innerHTML.toLowerCase().includes(search.value.toLowerCase())){
+                console.log(td[i].innerHTML)
                 productNotFound = false
+            } else {
+                document.querySelector('#row-' + CSS.escape(i)).style.display = 'none'
             }
         }
+
         if (productNotFound) {
             console.log('Product not found')
-        }
+        } 
     }
 })
 
@@ -27,24 +38,31 @@ async function displayData() {
     let data = await loadData();
     console.log(data)
     let tbody = document.querySelector('tbody')
-    for (let individual of data) {
+    tbody.style.display = ''
+
+    for (let i = 0; i < data.length; i++) {
         let tr = document.createElement('tr')
+        tr.id = `row-${i}`
         tbody.appendChild(tr)
+        
         let td1 = document.createElement('td')
-        td1.innerHTML = individual.product_name
-        td1.className = 'col-2'
+        td1.innerHTML = data[i].product_name
+        td1.className = 'product-name'
+        
         let td2 = document.createElement('td')
-        let split = individual.active_ingredients.split('&&')
+        let split = data[i].active_ingredients.split('&&')
         if (Array.isArray(split)){
             for (let i of split){
                 i = i.toLowerCase();
                 td2.innerHTML += i + '<br>'
             }
         }
-        td2.classList.add('text-capitalize','col-9')
+        td2.classList.add('text-capitalize','api')
+        
         let td3 = document.createElement('td')
-        td3.innerHTML = individual.forensic_classification
-        td3.className = 'col-1'
+        td3.innerHTML = data[i].forensic_classification
+        td3.className = 'classification'
+        
         tr.appendChild(td1)
         tr.appendChild(td2)
         tr.appendChild(td3)
